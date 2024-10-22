f8c9b9fb5f5d156896dd24db7d562f09b75b7f72
jgurung <jgurun@pointpark.edu>
2024-10-22 18:07:10 -0400

    i have moved initial node modules and .json files

--- CMPS364/module7/filterById.js
+++ CMPS364/module7/filterById.js
@@ -35,3 +35,20 @@ app.get('/books', (req, res) => {
     // res.json({mssg: "welcome to my MongoDB API"})
 })
 
+// // Route to get a book by _id
+app.get('/books/:id', (req, res) => {
+    const bookId = parseInt(req.params.id); // Parse the id as an integer
+
+    db.collection('books')
+        .findOne({ _id: bookId }) // Query using integer _id
+        .then((book) => {
+            if (book) {
+                res.status(200).json(book);
+            } else {
+                res.status(404).json({ error: 'Book not found' });
+            }
+        })
+        .catch((err) => {
+            res.status(500).json({ error: 'Could not fetch the book' });
+        });
+});
