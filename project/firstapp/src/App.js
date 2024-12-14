22826d78bdb1a74d5ea902d39c0dd98ed110dbd6
jgurung <jgurun@pointpark.edu>
2024-12-13 23:49:59 -0500

    made a first mobile app

---
+++ project/firstapp/src/App.js
@@ -0,0 +1,11 @@
+import '.App.css';
+import './components/hola'
+
+function App() {
+  return (
+    <div className="App">
+      <Hola/>
+      <Welcom/>
+    </div>
+  )
+}
---
+++ project/firstapp/src/components/hola.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Hola() {
+    return <h1>Hellow World</h1>
+}
+
+export default Hola
